de215705eb42ed1ebda8b628750e6cc5d2e0fe46
r3dact3d <redhatbrady@gmail.com>
2024-12-12 15:59:29 -0600

    add output for 2nd node

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,13 +1,24 @@
 # Output values
 
-output "public_ip" {
-  value = aws_instance.aap_instance.public_ip
+output "public_ip_1" {
+  value = aws_instance.aap_instance_1.public_ip
 }
 
-output "public_fqdn" {
-  value = aws_instance.aap_instance.public_dns
+output "public_fqdn_1" {
+  value = aws_instance.aap_instance_1.public_dns
 }
 
-output "instance_id" {
-  value = aws_instance.aap_instance.id
+output "instance_id_1" {
+  value = aws_instance.aap_instance_1.id
+}
+output "public_ip_2" {
+  value = aws_instance.aap_instance_2.public_ip
+}
+
+output "public_fqdn_2" {
+  value = aws_instance.aap_instance_2.public_dns
+}
+
+output "instance_id_2" {
+  value = aws_instance.aap_instance_2.id
 }
